ccf1a733646c46ae01eecf5ce9cef926a6952e69
Baldi627 <82242487+Baldi627@users.noreply.github.com>
2023-07-04 14:04:49 -0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,4 @@
+
+
+    window.silex = window.silex || {}
+    window.silex.data = {"site":{"width":967},"pages":[{"id":"page-home","displayName":"Home","link":{"linkType":"LinkTypePage","href":"#!page-home"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-contact","displayName":"Contact","link":{"linkType":"LinkTypePage","href":"#!page-contact"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-projects","displayName":"Projects","link":{"linkType":"LinkTypePage","href":"#!page-projects"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true,"opened":false},{"id":"page-unions","displayName":"Unions","link":{"linkType":"LinkTypePage","href":"#!page-unions"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true}]}
